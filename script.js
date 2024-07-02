0d74067a23cd7ea6746d5c70d7cc206dfbe8aced
giacomohrd <giacomo.accardi.lavoro@gmail.com>
2024-07-02 15:24:20 +0200

    [UPD] There are now some instructions inside the js file

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+L'bboettivo di oggi è quello di creare una lista della spessa nella quale l'utente può inserire i prodotti.
+
+1. Creare un array vuoto nella quale conservare le informazioni inserite dall'utente
+2. creare un loop nella quale venga più volte chiesto (tramite prompt) all'utente di inserire qualcosa all'interno della lista
+    >>creare la possibilità di uscire dal ciclo tramite l'inserimento della parola "stop" all'interno del prompt
+3. aggiungere gli elementi inseriti sall'utente all'interno del DOM
+-----------------------------------------------------------------------------
+*/
+
